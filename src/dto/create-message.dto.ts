918fab8c9a2b81bbdf9d3724d1079733ef8cadaf
theodevoid <voidfnc9@gmail.com>
2024-03-13 12:33:44 +0700

    lowercase username

--- src/dto/create-message.dto.ts
+++ src/dto/create-message.dto.ts
@@ -1,9 +1,11 @@
+import { Transform } from 'class-transformer';
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateMessageDTO {
   @IsString()
   @MinLength(4)
   @MaxLength(16)
+  @Transform(({ value }) => (value as string).toLowerCase())
   readonly toUsername: string;
 
   @IsString()
